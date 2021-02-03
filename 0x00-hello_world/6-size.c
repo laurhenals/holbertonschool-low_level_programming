5adb29a4a00509026d1e5770649785ad236f5a1a
laurhenals <laurhenals@gmail.com>
2021-02-03 23:30:27 +0000

    6-size.c prueba

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-  printf("sizeof char is %zu\n", sizeof(char));
-  printf("sizeof int is %zu\n", sizeof(int));
-  printf("sizeof long int is %zu\n", sizeof(long int));
-  printf("size of long long int is %zu\n", sizeof(long long int));
-  printf("size of float is %zu\n", sizeof(float));
+  printf("sizeof char is %lu byte(s)\n", sizeof(char));
+  printf("sizeof int is %lu byte(s)\n", sizeof(int));
+  printf("sizeof long int is %lu byte(s)\n", sizeof(long int));
+  printf("size of long long int is %lu byte(s)\n", sizeof(long long int));
+  printf("size of float is %lu byte(s)\n", sizeof(float));
   return (0);
 }
