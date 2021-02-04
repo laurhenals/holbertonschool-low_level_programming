689b077af0e746ad0e81b874e797bdd6e25941eb
laurhenals <laurhenals@gmail.com>
2021-02-04 22:00:38 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,10 @@
  */
 int main(void)
 {
-  char c = 'a';
-  while (c <= 'z')
+  char c;
+  for (c = 'a'; c <= 'z'; c++)
     {
       putchar(c);
-      c++;
     }
   putchar('\n');
   return (0);
