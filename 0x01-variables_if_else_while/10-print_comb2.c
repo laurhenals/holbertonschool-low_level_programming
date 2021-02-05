c71a4014515ee98e9e6cc50708f6f023aecc674a
laurhenals <laurhenals@gmail.com>
2021-02-05 00:06:06 +0000

    10-print_comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int c, d;
+for (c = 0; c < 10; c++)
+{
+for (d = 0; d < 10; d++)
+{
+putchar((c % 10) + '0');
+putchar((d % 10) + '0');
+
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
