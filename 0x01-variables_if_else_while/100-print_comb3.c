5519bbb62c1b344359db0eb3f91a501cecbceb0e
Laura Rhenals <laurhenals@gmail.com>
2021-02-05 11:16:56 -0500

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,16 @@ int main(void)
 {
 int a;
 int b;
-for (a = 48 ; a < 58 ; a++)
+for (a = 0 ; a < 9 ; a++)
 {
-	for (b = a + 1 ; b < 58 ; b++)
+	for (b = a + 1 ; b < 9 ; b++)
 	{
 		putchar (a);
 		putchar (b);
-		if (a != 56 || b != 57)
+		if (a != 8 || b != 9)
 		{
-			putchar (44);
-			putchar (32);
+			putchar (',');
+			putchar (' ');
 		}
 	}
 }
