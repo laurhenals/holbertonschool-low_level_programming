e605e49655382f7866d03ada821cef3a0480922d
Laura Rhenals <laurhenals@gmail.com>
2021-02-04 19:12:38 -0500

    Update 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,21 +7,24 @@
  */
 int main(void)
 {
-int c, d;
-for (c = 0; c < 10; c++)
-{
-for (d = 0; d < 10; d++)
-{
-putchar((c % 10) + '0');
-putchar((d % 10) + '0');
-
-if(c == 9 && d == 9)
-continue;
-
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+  int c, d;
+  
+  for (c = 0; c < 10; c++)
+  {
+    for (d = 0; d < 10; d++)
+    {
+      putchar((c % 10) + '0');
+      putchar((d % 10) + '0');
+      
+      if(c == 9 && d == 9)
+        continue;
+      
+      putchar(',');
+      putchar(' ');
+    }
+  }
+  
+  putchar('\n');
+  
+  return (0);
 }
